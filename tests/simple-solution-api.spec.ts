ef97e4f29582d0b7caf86bc8d947809ac01cdeaa
Prihhan <prihhan@gmail.com>
2025-12-17 13:37:14 +0200

    negative number instead of negative case

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -32,7 +32,9 @@ test('Negative number should receive code 400', async ({ request }) => {
   expect(response.status()).toBe(400)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
+/////////////////////////////// POST case ///////////////////////////////
+//positive case
+test('post order with correct data should receive code 200', async ({ request }) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN',
@@ -50,4 +52,159 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
+})
+
+//negative case
+test('post order with correct data should receive code 400', async ({ request }) => {
+  // prepare request body
+  const requestBody = {
+    status: 'OP', //adjusted parameter
+    courierId: '0',
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+
+
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: requestBody,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+
+////////////////////////////homework09 assignment////////////////////////
+// PUT endpoint
+test('update order with valid ID and valid API key should receive 200 OK', async ({ request }) => {
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 5,
+  }
+
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a PUT request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/5', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('update order with missing API key should receive 401 Unauthorized', async ({ request }) => {
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 5,
+  }
+
+  const requestHeaders = {
+    api_key: '',
+  }
+  // Send a POST request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/5', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('update order with empty request body should receive 400 Bad Request', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a PUT request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+// DELETE endpoint
+test('delete order with valid ID and valid API key should receive 204 No Content', async ({
+                                                                                            request,
+                                                                                          }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a DELETE request to the server
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.NO_CONTENT)
+})
+
+test('delete order with invalid API key should receive 401 Unauthorized', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '111',
+  }
+  // Send a DELETE request to the server
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('delete order with missing API key should receive 401 Unauthorized', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '',
+  }
+  // Send a DELETE request to the server
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+// GET endpoint
+test('login with valid username and password should receive 200 OK', async ({ request }) => {
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders', {
+    params: {
+      username: 'Andrei',
+      password: 'password123',
+    },
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('login with missing username and password should receive 500 Internal Server Error', async ({
+                                                                                                   request,
+                                                                                                 }) => {
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders', {})
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
 })
